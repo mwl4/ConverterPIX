c30ddffa1bbf95cd5f879f4e96db0649bb8b2951
mwl4 <mwl450@gmail.com>
2017-05-01 17:31:41 +0200

    Fixes.

---
+++ src/prerequisites.cpp
@@ -0,0 +1,12 @@
+/*********************************************************************
+ *           Copyright (C) 2017 mwl4 - All rights reserved           *
+ *********************************************************************
+ * File       : prerequisites.cpp
+ * Project    : ConverterPIX
+ * Developers : Michal Wojtowicz (mwl450@gmail.com)
+ 			  : Piotr Krupa (piotrkrupa06@gmail.com)
+ *********************************************************************/
+
+#include <prerequisites.h>
+
+/* eof */
